87503d815c5235e254901f13d9fc5b0ef91dbe22
Martin Rasmussen <martin.tr@live.dk>
2020-05-08 15:10:16 +0200

    Add files via upload

---
+++ pages/frontpage.php
@@ -0,0 +1,71 @@
+
+    <header class="index-head">
+      <div class="overlay"></div>
+      <video
+        playsinline="playsinline"
+        autoplay="autoplay"
+        muted="muted"
+        loop="loop"
+        preload="auto"
+      >
+        <source src="content/vid/minecraft_vid.mp4" type="video/mp4" />
+      </video>
+      <div class="container h-100">
+        <div class="d-flex h-100 text-center align-items-center">
+          <div class="w-100 text-white">
+            <h1 class="display-3 titel">Minecraft guide</h1>
+            <p class="lead mb-0">
+              A website for learning the world around Minecraft
+            </p>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section>
+      <h2 id="news">Minecraft news</h2>
+      <div class="row align-items-sm-center minecraft-update">
+        
+          <div class="col-md minecraft-pics">
+            <a href="https://www.minecraftforum.net/news/60771-java-edition-snapshot-20w18a" target="blank" ><img
+              src="content/img/2020-05-06_11.48.09.png"
+              class="img-fluid"
+              alt="Minecraft-billeder"
+            />
+            <div class="center-text">
+              <h3>Java Edition: Snapshot 20W18A</h3>
+              <p>
+                Redstoners! This is a snapshot for you to pay attention to!  Check out the new look and ways it interacts with the Minecraft world.
+              </p></a>
+            </div>
+          </div>
+        
+        <div class="col-md minecraft-pics">
+          <a href="https://www.minecraftforum.net/news/60770-beta-bedrock-1-16-0-58-xbox-one-windows-10-android" target="blank"><img
+            src="content/img/2020-05-06_11.51.15.png"
+            class="img-fluid"
+            alt="Minecraft-billeder"
+          />
+          <div class="center-text">
+            <h3>BETA: Bedrock 1.16.0.58</h3>
+            <p>
+              Another round of Bug Fixes for those playing Minecraft: BETA. Do you see a bug you've encountered fixed?
+            </p></a>
+          </div>
+        </div>
+
+        <div class="col-md minecraft-pics">
+          <a href="https://www.minecraftforum.net/news/60767-minecraft-with-rtx-beta-is-now-live" target="blank"><img
+            src="content/img/2020-05-06_14.57.40.png"
+            class="img-fluid"
+            alt="Minecraft-billeder"
+          />
+          <div class="center-text">
+            <h3>Minecraft RTX Beta is now live!</h3>
+            <p>
+              The RTX beta is now live, with pretty lights, shadows, and reflections for Minecraft worlds!
+            </p></a>
+          </div>
+        </div>
+      </div>
+    </section>
